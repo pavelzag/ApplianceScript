74d0c8e546f263d880de6382fd1606c4f48bd6e2
Pavel Zagalsky <pzagalsk@redhat.com>
2016-06-12 18:02:41 +0300

    Small fixes

--- sshConnector.py
+++ sshConnector.py
@@ -14,17 +14,12 @@ def get_token(hostname, port, username, password):
     for line in stdout:
         if str(line).__contains__('token'):
             token_id = extract_token(line)
-            # print(token_id)
-    # for line in stderr:
-    #     # print(line)
     command_begin = 'oc get -n management-infra secrets management-admin-token-' + token_id + ' --template'
     send_parameter = command_begin + r"""='{{.data.token}}' | base64 -d"""
     stdin, stdout, stderr = ssh.exec_command(send_parameter)
     for line in stdout:
         print('The token is: ' + line)
         token = line
-    # for line in stderr:
-    #      print('The toke is: ' + line)
     return token
 # token = get_token(hostname='pavel-openshift-master01.scl.lab.tlv.redhat.com', port=22, username='root', password='qum5net')
 # print(token)
